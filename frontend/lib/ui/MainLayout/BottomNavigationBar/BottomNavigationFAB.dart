d7280cb5bb805659f6ae6e5bae287c6279d0c886
yashdhume <yash_dhume@yahoo.ca>
2020-10-30 16:40:30 -0400

    Navigates to Actual Pages rather than repeated Pages

--- frontend/lib/ui/MainLayout/BottomNavigationBar/BottomNavigationFAB.dart
+++ frontend/lib/ui/MainLayout/BottomNavigationBar/BottomNavigationFAB.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:size_hub/ui/Pages/CameraPage.dart';
 
 class BottomNavigationFAB extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FloatingActionButton(
-      onPressed: () {},
+      onPressed: () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => CameraPage()));
+      },
       child: Icon(
         Icons.camera_alt,
       ),
--- frontend/lib/ui/MainLayout/MainLayout.dart
+++ frontend/lib/ui/MainLayout/MainLayout.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:size_hub/ui/MainLayout/BottomNavigationBar/BottomNavigationIconButton.dart';
 import 'package:size_hub/ui/Pages/ExplorePage.dart';
-import 'package:size_hub/ui/Pages/SplashPage.dart';
-import 'package:size_hub/ui/Widgets/FirebaseTest.dart';
-
+import 'package:size_hub/ui/Pages/MapPage.dart';
+import 'package:size_hub/ui/Pages/ProfilePage.dart';
+import 'package:size_hub/ui/Pages/SearchPage.dart';
 import 'BottomNavigationBar/BottomNavigation.dart';
 import 'BottomNavigationBar/BottomNavigationFAB.dart';
+import 'package:size_hub/ui/MainLayout/BottomNavigationBar/BottomNavigationIconButton.dart';
 
 class MainLayout extends StatefulWidget {
   MainLayout({Key key}) : super(key: key);
@@ -18,9 +18,9 @@ class _MainLayoutState extends State<MainLayout> {
   int _currentIndex = 0;
   List<Widget> _children = [
     ExplorePage(),
-    SplashPage(),
-    FirebaseTest(),
-    ExplorePage(),
+    SearchPage(),
+    MapPage(),
+    ProfilePage()
   ];
 
   void onIconButtonPress(int index) {
