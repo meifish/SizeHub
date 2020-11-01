ec8d52a51826818e86b43fdc11072df3a0decd3c
yashdhume <yash_dhume@yahoo.ca>
2020-11-01 16:42:14 -0500

    .

--- frontend/lib/ui/animations/RotateAnimation.dart
+++ frontend/lib/ui/animations/RotateAnimation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math' show pi;
+
 const Duration defaultDuration = Duration(seconds: 2);
 
 class RotateAnimation extends StatefulWidget {
@@ -7,9 +8,10 @@ class RotateAnimation extends StatefulWidget {
   final Duration duration;
   final Duration delay;
 
-  RotateAnimation({@required this.child,
-    this.duration = defaultDuration,
-    this.delay = Duration.zero});
+  RotateAnimation(
+      {@required this.child,
+      this.duration = defaultDuration,
+      this.delay = Duration.zero});
 
   @override
   _RotateAnimationState createState() => _RotateAnimationState();
@@ -39,14 +41,14 @@ class _RotateAnimationState extends State<RotateAnimation>
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-        animation: _controller,
-        child: widget.child,
-        builder: (_, child) {
-          return Transform.rotate(
-            angle: _controller.value*pi*2,
-            child: child,
-          );
-        },
+      animation: _controller,
+      child: widget.child,
+      builder: (_, child) {
+        return Transform.rotate(
+          angle: _controller.value * pi * 2,
+          child: child,
+        );
+      },
     );
   }
 }
