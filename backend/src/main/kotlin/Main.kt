1722c5554d13cfae38b1d6cf89bf31fef658d8ce
Vincent <vincentpetergalloro@gmail.com>
2020-11-09 00:57:34 -0500

    Added Dummy Data generator to populate the firestore DB

--- backend/src/main/kotlin/Main.kt
+++ backend/src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 package src
 
 import src.database.Database
+import src.database.DummyData
 
 fun main() {
     val database = Database()
+
+    DummyData(database.publicDb).generate()
 }
---
+++ backend/src/main/kotlin/database/DummyData.kt
@@ -0,0 +1,43 @@
+package src.database
+
+import src.data.*
+import src.database.dbitems.User
+import kotlin.random.Random
+
+class DummyData(private val publicDb: PublicDb) {
+
+    fun generate(){
+        val gucciBrand = publicDb.searchBrandByName("Gucci")
+            ?: publicDb.createBrand(BrandData("Gucci", "https://www.gucci.com/ca/en/"))
+        val supremeBrand = publicDb.searchBrandByName("Supreme")
+            ?: publicDb.createBrand(BrandData("Supreme", "https://www.streetwearofficial.com/collections/supreme"))
+        val theGapBrand = publicDb.searchBrandByName("The Gap")
+            ?: publicDb.createBrand(BrandData("The Gap", "https://www.gapcanada.ca/"))
+
+        listOf(gucciBrand, supremeBrand, theGapBrand).forEach { brand ->
+            listOf("Red", "Green", "Blue").forEach { color ->
+                listOf("T-Shirt", "Pants").forEach { item ->
+                    val name = "$color $item"
+                    if(publicDb.searchItemByNameAndBrand(brand.id, name) == null) {
+                        publicDb.createClothingItem(ClothingItemData(brand.id, name, emptyList()))
+                    }
+                }
+            }
+        }
+
+        val jeb = publicDb.searchUserByName("Jeb24") ?: publicDb.createUser(UserData("Jeb24"))
+        val joe = publicDb.searchUserByName("Joe1999") ?: publicDb.createUser(UserData("Joe1999"))
+
+        repeat(5){
+            generateRandomPost(listOf(jeb, joe).random()!!)
+        }
+    }
+
+    fun generateRandomPost(user: User){
+        val brand = publicDb.getBrands().random()
+        val item = publicDb.searchItemsByBrand(brand.id).random()
+        val measurementsData = UserMeasurementsData(Random.nextInt(100, 200), Random.nextInt(100, 200))
+
+        publicDb.createPost(PostData(user.id, item.id, measurementsData, emptyList(), "test comment"))
+    }
+}
