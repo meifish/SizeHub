793cde2a28c9ca1b6befd95ce9b9a342c076ec91
yashdhume <yash_dhume@yahoo.ca>
2020-11-30 15:21:30 -0500

    Refactored Code so Scrollable card can be reused

---
+++ frontend/lib/ui/Widgets/Common/ScrollableCard.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:size_hub/ui/animations/SlideInAnimation.dart';
+
+class ScrollableCard extends StatelessWidget {
+  final Widget child;
+  final double initialChildSize;
+  final double minChildSize;
+  final double maxChildSize;
+  final double radius;
+  final Color backgroundColor;
+
+  const ScrollableCard(
+      {Key key,
+      @required this.child,
+      this.backgroundColor = Colors.white,
+      this.initialChildSize = 0.2,
+      this.minChildSize = 0.1,
+      this.maxChildSize = 1,
+      this.radius = 25})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DraggableScrollableSheet(
+      initialChildSize: initialChildSize,
+      minChildSize: minChildSize,
+      maxChildSize: maxChildSize,
+      builder: (BuildContext context, ScrollController scrollController) {
+        return SingleChildScrollView(
+          controller: scrollController,
+          child: SlideInAnimation(
+              offset: Offset(0, 1000),
+              child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: backgroundColor,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(radius),
+                      topRight: Radius.circular(radius),
+                    ),
+                  ),
+                  child: child)),
+        );
+      },
+    );
+  }
+}
--- frontend/lib/ui/Widgets/ExplorePageWidgets/DetailedPost/DetailedPostScrollableCard.dart
+++ frontend/lib/ui/Widgets/ExplorePageWidgets/DetailedPost/DetailedPostScrollableCard.dart
@@ -1,39 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:size_hub/data/UserMeasurementsData.dart';
+import 'package:size_hub/ui/Widgets/Common/ScrollableCard.dart';
 import 'package:size_hub/ui/Widgets/ExplorePageWidgets/DetailedPost/DetailedPostScrollableCardContent.dart';
-import 'package:size_hub/ui/animations/SlideInAnimation.dart';
 
 class DetailedPostScrollableCard extends StatelessWidget {
-
   final UserMeasurementsData userMeasurementData;
   final List<String> photoUrls;
 
-  const DetailedPostScrollableCard({Key key, this.userMeasurementData, this.photoUrls}) : super(key: key);
+  const DetailedPostScrollableCard(
+      {Key key, this.userMeasurementData, this.photoUrls})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      initialChildSize: 0.2,
-      minChildSize: 0.1,
-      maxChildSize: 1,
-      builder: (BuildContext context, ScrollController scrollController) {
-        return SingleChildScrollView(
-          controller: scrollController,
-          child: SlideInAnimation(
-              offset: Offset(0, 1000),
-              child: Container(
-                 // height: 3000,
-                  width: MediaQuery.of(context).size.width,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(24),
-                      topRight: Radius.circular(24),
-                    ),
-                  ),
-                  child: DetailedPostScrollableCardContent(photoUrls: photoUrls, userMeasurementData: userMeasurementData,))),
-        );
-      },
-    );
+    return ScrollableCard(
+        child: DetailedPostScrollableCardContent(
+            photoUrls: photoUrls, userMeasurementData: userMeasurementData));
   }
 }
