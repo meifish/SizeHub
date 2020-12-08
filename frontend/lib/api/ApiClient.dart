21ea7a4d348e9f38217870185b5dc226d2ea1eda
Vincent <vincentpetergalloro@gmail.com>
2020-12-08 16:51:51 -0500

    Add new functions to ApiClient

--- frontend/lib/api/ApiClient.dart
+++ frontend/lib/api/ApiClient.dart
@@ -4,8 +4,17 @@ import 'dart:convert';
 import 'package:dio/dio.dart';
 import 'package:size_hub/api/args/CreateCommentArgs.dart';
 import 'package:size_hub/data/CommentData.dart';
+import 'package:size_hub/data/DetailedPostData.dart';
+import 'package:size_hub/data/PostPreviewData.dart';
+import 'package:size_hub/data/PublicUserProfileData.dart';
 
 import 'ApiErrorResponse.dart';
+
+enum PostSort {
+  RECENT,
+  LIKES,
+  COMMENTS,
+}
 
 class ApiClient{
 
@@ -18,7 +27,7 @@ class ApiClient{
       });
   static Dio dio = Dio(options);
 
-  Map<String, dynamic> validateResponse(Response response){
+  Map<String, dynamic> _validateResponse(Response response){
     Map<String, dynamic> res = jsonDecode(response.data);
     if(response.statusCode == 500){
       print("SERVER ERROR");
@@ -33,16 +42,16 @@ class ApiClient{
     return res;
   }
 
-  Future<Map<String, dynamic>> makeGetRequest(String path, Map<String, dynamic> args) async {
+  Future<Map<String, dynamic>> _makeGetRequest(String path, Map<String, dynamic> args) async {
     try {
-      return validateResponse(await dio.get("http://10.0.2.2:3000" + path, queryParameters: args));
+      return _validateResponse(await dio.get("http://10.0.2.2:3000" + path, queryParameters: args));
     }
     catch(e){ print(e); return null; }
   }
 
-  Future<Map<String, dynamic>> makePostRequest(String path, dynamic data) async {
+  Future<Map<String, dynamic>> _makePostRequest(String path, dynamic data) async {
     try {
-      return validateResponse(await dio.post("http://10.0.2.2:3000" + path, data: data));
+      return _validateResponse(await dio.post("http://10.0.2.2:3000" + path, data: data));
     }
     catch(e){ print(e); return null; }
   }
@@ -50,14 +59,32 @@ class ApiClient{
   //API CALLS
   
   Future<CommentData> createComment(CreateCommentArgs args) async{
-    Map<String, dynamic> response = await makePostRequest("/createComment", args);
+    Map<String, dynamic> response = await _makePostRequest("/createComment", args);
     if(response == null) return null;
     return CommentData.fromJson(response);
   }
 
   Future<List<CommentData>> getReplies(String postId) async{
-    Map<String, dynamic> response = await makeGetRequest("/replies", {"postId": postId});
+    Map<String, dynamic> response = await _makeGetRequest("/replies", {"postId": postId});
     if(response == null) return null;
     return (response["comments"] as List<dynamic>).map((e){ return CommentData.fromJson(e); }).toList();
+  }
+
+  Future<DetailedPostData> getDetailedPost(String postId) async{
+    Map<String, dynamic> response = await _makeGetRequest("/post", {"postId": postId});
+    if(response == null) return null;
+    return DetailedPostData.fromJson(response);
+  }
+
+  Future<PublicUserProfileData> getProfile(String userId) async{
+    Map<String, dynamic> response = await _makeGetRequest("/profile", {"userId": userId});
+    if(response == null) return null;
+    return PublicUserProfileData.fromJson(response);
+  }
+
+  Future<List<PostPreviewData>> getPostsByUser(String userId, PostSort sort) async{
+    Map<String, dynamic> response = await _makeGetRequest("/postsByUser", {"userId": userId, "sort": sort.toString().split(".").last});
+    if(response == null) return null;
+    return (response["posts"] as List<dynamic>).map((e){ return PostPreviewData.fromJson(e); }).toList();
   }
 }
